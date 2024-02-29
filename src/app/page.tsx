f6e6b487b1843856872f3e253e293e98758673ec
1F10220082 <s1f102200828@iniad.org>
2024-03-01 00:46:47 +0900

    文言の変更

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (<div className="Home">
       <h2>ポートフォリオです。</h2>
-      <p>左のメニューをクリックするとそれぞれのページに書き換わります</p>
+      <p>メニューからページを選択してください</p>
+      <p>このサイトは、Next.jsで開発しました</p>
   </div>);
 }
