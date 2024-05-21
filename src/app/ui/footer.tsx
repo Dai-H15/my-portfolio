1aa0f9b48ec1be3f38e01b466203067426fc542f
1F10220082 <s1f102200828@iniad.org>
2024-05-21 13:52:29 +0900

    一応注意書きを掲載

--- src/app/ui/footer.tsx
+++ src/app/ui/footer.tsx
@@ -1,6 +1,7 @@
 export default function Footer(){
     return(<div className="Footer text-center border-top">
-            <p>2024  Daichi Matsumoto <a href="mailto:s1f102200828@iniad.org?">お問い合わせ</a></p>
+            <small>2024  Daichi Matsumoto <a href="mailto:s1f102200828@iniad.org?">お問い合わせ</a></small><br/>
+            <small>このサイトは、アクセス数管理の目的でGoogle Analyticsを使用しています。</small>
             </div>
         )
 }
